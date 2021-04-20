97ba4d3cecc304dca7b1c61d9c4a4c449c7daaa0
Lucas Cardoso Roscoe <lcardoso@IMASUL.MS>
2021-04-20 14:31:00 -0300

    Config API

---
+++ WebApi/Controllers/VacinadoController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class VacinadoController : ApiController
+    {
+        // GET: api/vacinado
+        public List<Vacinado> Get()
+        {
+            Vacinado vacinado = new Vacinado();
+            return vacinado.ListaVacinados();
+        }
+
+        // GET: api/vacinado/5
+        public Vacinado Get(int id)
+        {
+            Vacinado vacinado = new Vacinado();
+            return vacinado.ListaVacinados().Where(x => x.Sid ==id).FirstOrDefault();
+        }
+
+        // GET: api/vacinado/5
+        public Vacinado Get(string nome)
+        {
+            Vacinado vacinado = new Vacinado();
+            return vacinado.ListaVacinados().Where(x => x.Snome == nome).FirstOrDefault();
+        }
+
+
+        // POST: api/vacinado
+        public List<Vacinado> Post(Vacinado vacinado)
+        {
+            Vacinado _vacinado = new Vacinado();
+            _vacinado.Inserir(vacinado);
+            return _vacinado.ListaVacinados();
+
+        }
+
+        // PUT: api/vacinado/5
+        public List<Vacinado> Put(int id, Vacinado vacinado)
+        {
+            Vacinado _vacinado = new Vacinado();
+            _vacinado.Atualizar(id, vacinado);
+            return _vacinado.ListaVacinados();
+
+        }
+
+        // DELETE: api/vacinado/5
+        public void Delete(int id)
+        {
+            Vacinado _vacinado = new Vacinado();
+            _vacinado.Deletar(id);
+           
+        }
+    }
+}
---
+++ WebApi/Models/Vacinado.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebApi.Models
+{
+    public class Vacinado
+    {
+        public int Sid { get; set; }
+        public string Snome { get; set; }
+        public string Scpf { get; set; }
+        public string Stelefone { get; set; }
+        public string Semail { get; set; }
+        public int Sidade { get; set; }
+
+        public List<Vacinado> ListaVacinados()
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            var listaVacinados = JsonConvert.DeserializeObject<List<Vacinado>>(json);
+
+            return listaVacinados;
+        }
+        public bool ReescreverArquivo(List<Vacinado> listaVacinados)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\base.json");
+            var json = JsonConvert.SerializeObject(listaVacinados, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+            return true;
+
+        }
+        public Vacinado Inserir(Vacinado vacinado)
+        {
+            var ListaVacinados = this.ListaVacinados();
+            var maxId = ListaVacinados.Max(vacinados => vacinado.Sid);
+            vacinado.Sid = maxId + 1;
+            ListaVacinados.Add(vacinado);
+            ReescreverArquivo(ListaVacinados);
+            return vacinado;
+
+        }
+        public Vacinado Atualizar(int id, Vacinado vacinado)
+        {
+            var ListaVacinados = this.ListaVacinados();
+            var itemIndex = ListaVacinados.FindIndex(p => p.Sid == id);
+            if (itemIndex >= 0)
+            {
+                vacinado.Sid = id;
+                ListaVacinados[itemIndex] = vacinado;
+
+            }
+            else
+            {
+                return null;
+            }
+            ReescreverArquivo(ListaVacinados);
+            return vacinado;
+        }
+        public bool Deletar(int id)
+        {
+            var ListaVacinados = this.ListaVacinados();
+            var itemIndex = ListaVacinados.FindIndex(p => p.Sid == id);
+            if (itemIndex >= 0)
+            {
+                ListaVacinados.RemoveAt(itemIndex);
+
+            }
+            else
+            {
+                return false;
+            }
+            ReescreverArquivo(ListaVacinados);
+            return true;
+        }
+    }
+}
